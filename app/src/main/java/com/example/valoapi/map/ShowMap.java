0346790feb097ba4b4d73d08dc7abe0c69820501
EVIL HYDRA <abhidnya09@gmail.com>
2022-09-10 19:25:02 +0530

    added fullscreen

--- app/src/main/java/com/example/valoapi/map/ShowMap.java
+++ app/src/main/java/com/example/valoapi/map/ShowMap.java
@@ -3,6 +3,7 @@ package com.example.valoapi.map;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
@@ -14,7 +15,7 @@ ImageView mapimage;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_map);
-
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         mapimage = findViewById(R.id.mapdisplay);
 
         String map = getIntent().getStringExtra("mapimage");
