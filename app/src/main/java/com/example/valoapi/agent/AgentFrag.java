8941c2910d8196e74ae279dd8bd79496040eb884
EVIL HYDRA <abhidnya09@gmail.com>
2022-09-19 12:20:48 +0530

    apk

--- app/src/main/java/com/example/valoapi/agent/AgentFrag.java
+++ app/src/main/java/com/example/valoapi/agent/AgentFrag.java
@@ -1,9 +1,11 @@
 package com.example.valoapi.agent;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -44,6 +46,7 @@ public class AgentFrag extends Fragment {
         @Override
         public void onResponse(Call<AgentPojo> call, Response<AgentPojo> response) {
             if(response.isSuccessful()){
+                Log.v("Tag","the response is "+response.body().toString());
                 dataArrayList.addAll(response.body().getData());
                 agentAdapter.notifyDataSetChanged();
             }
